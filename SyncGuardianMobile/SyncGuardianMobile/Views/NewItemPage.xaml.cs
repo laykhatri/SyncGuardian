35013d7e517e0ef3e9760a31927e91dae05e5822
Lay Khatri <laykhatri@hotmail.com>
2023-09-24 22:08:21 +0530

    8 - Created scafolded project with Tab UI.

---
+++ SyncGuardianMobile/SyncGuardianMobile/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using SyncGuardianMobile.Models;
+using SyncGuardianMobile.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SyncGuardianMobile.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
