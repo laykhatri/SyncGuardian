a499b3c2f5ba2200355a31424bf79cd88ade007e
Lay Khatri <laykhatri@hotmail.com>
2023-09-25 20:05:32 +0530

    6 - Added base for MVVM architecture

--- SyncGuardianWpf/App.xaml.cs
+++ SyncGuardianWpf/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +15,30 @@ namespace SyncGuardianWpf
     /// </summary>
     public partial class App : Application
     {
+        private IHost _host;
+
+        public App()
+        {
+            _host = Host.CreateDefaultBuilder()
+                .ConfigureServices((context, services) =>
+                {
+                    DiConfig.ConfigureDependencies(services);
+                })
+                .Build();
+        }
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            await _host.StartAsync();
+            var mainWindow = _host.Services.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+            await _host.StopAsync();
+        }
     }
 }
