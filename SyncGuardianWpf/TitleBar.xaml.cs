71b37578c74154ac5e5535ea6ab0dd0ec6a0c290
Lay Khatri <laykhatri@hotmail.com>
2023-10-14 20:30:25 +0530

    42 - Partial commit

---
+++ SyncGuardianWpf/TitleBar.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Controls;
+
+namespace SyncGuardianWpf
+{
+    /// <summary>
+    /// Interaction logic for TitleBar.xaml
+    /// </summary>
+    public partial class TitleBar : UserControl
+    {
+
+        public TitleBar()
+        {
+            InitializeComponent();
+        }
+    }
+}
