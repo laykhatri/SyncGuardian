3acd5b71a27c7fadb5312c9f28d675c90dcb5f23
Lay Khatri <laykhatri@hotmail.com>
2024-03-08 08:07:45 +0530

    Implemented DI for Avalonia UI and changed location of Readme and Contributing files in solution

--- SyncGuardian.Desktop/App.axaml.cs
+++ SyncGuardian.Desktop/App.axaml.cs
@@ -1,18 +1,25 @@
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using Microsoft.Extensions.Hosting;
 
 namespace SyncGuardian.Desktop;
 
 public partial class App : Application
 {
+    private IHost? _host;
     public override void Initialize()
     {
+        _host = Host.CreateDefaultBuilder().ConfigureServices((context, services) =>
+        {
+            Bootstrapper.RegisterServices(services);
+        }).Build();
         AvaloniaXamlLoader.Load(this);
     }
 
-    public override void OnFrameworkInitializationCompleted()
+    public override async void OnFrameworkInitializationCompleted()
     {
+        await _host!.StartAsync();
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
             desktop.MainWindow = new MainWindow();
@@ -20,4 +27,5 @@ public partial class App : Application
 
         base.OnFrameworkInitializationCompleted();
     }
+
 }
---
+++ SyncGuardian.Desktop/Bootstrapper.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace SyncGuardian.Desktop
+{
+    public class Bootstrapper
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+
+        }
+    }
+}
