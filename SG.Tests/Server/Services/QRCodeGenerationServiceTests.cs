dcd0f8f82066e302ea78c3bcbffae18e8d15a052
Lay Khatri <laykhatri@hotmail.com>
2024-05-01 08:27:48 +0530

    57 - Added test cases for SG.Server.Services

---
+++ SG.Tests/Server/Services/QRCodeGenerationServiceTests.cs
@@ -0,0 +1,40 @@
+﻿using FakeItEasy;
+using Serilog;
+using SG.Server.Services.Interfaces;
+using System.Text.Json;
+
+namespace SG.Tests.Server.Services
+{
+    [TestFixture]
+    public class QRCodeGenerationServiceTests
+    {
+        private IQRCodeGenerationService _qrCodeGenerationService;
+        private ILogger _logger;
+
+        [SetUp]
+        public void Setup()
+        {
+            _logger = A.Fake<ILogger>();
+            _qrCodeGenerationService = A.Fake<IQRCodeGenerationService>();
+        }
+
+        [Test]
+        public void Validate_QRCodeGenerated()
+        {
+            var testData = new { column1 = 1, column2 = 2 };
+            var qrCode = _qrCodeGenerationService.GenerateQRCodeImage(JsonSerializer.Serialize(testData));
+
+            Assert.IsNotNull(qrCode);
+        }
+
+        [Test]
+        public void Validate_QRCodeNotBeingGenerated()
+        {
+            var testData = "helloworld";
+            var qrCode = _qrCodeGenerationService.GenerateQRCodeImage(JsonSerializer.Serialize(testData));
+
+            Assert.IsNotNull(qrCode);
+            Assert.IsTrue(qrCode.Length == 0);
+        }
+    }
+}
